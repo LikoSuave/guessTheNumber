428fc749c43efc405d2f6c88d54360fb82f6d21a
LikoSuave <mjmartin1997@gmail.com>
2020-12-14 15:08:25 -0500

    Created a for loop. Imported random.

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,2 +1,23 @@
-msg = "Hello!"
-print(msg)
+# This is a guessing game
+import random
+
+secretNumber = random.randint(1,20)
+print('Im thinking of a number between 1 and 20')
+
+# Ask the player to guess the number
+
+for guessesTaken in range(1,7):
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break # This condition is the correct guess
+
+if guess == secretNumber:
+    print('Good job! You guessed my number in' + str(guessesTaken) + ' guesses!')
+else:
+    print('Nope. The number I was thinking of was' + str(secretNumber)) 
